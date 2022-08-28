8500706d227fed20ff37462ef3b2f68532041019
= <=>
2022-08-28 17:32:04 +0530

    stylesheet fixed

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Hero from "/components/Hero";
 import Footer from "/components/Footer";
 import {IoMdPlay} from "react-icons/io";
